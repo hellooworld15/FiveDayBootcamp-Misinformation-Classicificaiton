08a189c4bdef10bf778ca580ba32ce3b8bf759aa
Aditya Pillai <adityaa.pillai17@gmail.com>
2022-02-10 15:10:31 +1100

    Working metrics classifier

--- BackEnd/algorithm.py
+++ BackEnd/algorithm.py
@@ -9,6 +9,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver import ChromeOptions
 from selenium.common.exceptions import NoSuchElementException
 from oauth2client.service_account import ServiceAccountCredentials
+from spellchecker import SpellChecker
+from bs4 import BeautifulSoup
+from htmldate import find_date
+from datetime import datetime as dt
+
 
 
 # Take input from front-end
@@ -37,7 +42,6 @@ def has_trusted_domain(link):
   trusted_domains = ['.edu', '.org', '.gov']
   for i in range(0, len(trusted_domains)):
     if trusted_domains[i] in link:
-      print("Trusted.")
       return True
   return False
 
@@ -49,19 +53,41 @@ def has_external_sources(link):
     return False
 
 def has_updated(link):
-  num = random.randint(0, 1)
-  if num <= 0.5:
+  updated_date = dt.strptime(find_date(link), "%Y-%m-%d")
+  threshold = dt.strptime("2017-02-10", "%Y-%m-%d")
+  difference = updated_date - threshold
+  print(difference)
+  if updated_date > threshold:
     return True
-  else:
-    return False
+  return False
 
 def has_typo_errors(link):
-  #API not returning correct values
+  r = requests.get(link)
+  r.text
+  #print(r.text)
+  soup = BeautifulSoup(r.text, 'html.parser')
+  spell = SpellChecker()
   num = random.randint(0, 1)
-  if num <= 0.5:
-    return True
+  # find those words that may be misspelled
+  # Input will be here
+  misspelled = spell.unknown([soup.get_text()])
+  TypoNo = 0
+  #print('Typos Detected:', misspelled)
+  for word in misspelled:
+      # Get the one most likely answer
+      #print('Typo Correction', TypoNo+1, ':', spell.correction(word))
+      TypoNo += 1
+
+  #print('There are', TypoNo, 'typos')
+  # Boolean Test
+  if TypoNo > 5:
+      m5 = True
   else:
+      m5 = False
+  if num <= 0.8:
     return False
+  else:
+    return True
 
 ### MODEL ###
 
