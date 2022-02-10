a9795f93bf2dd70070d867ee453c5970c5f50780
Jasmine Warner <hellooworld.warner@gmail.com>
2022-02-11 01:45:25 +1100

    Adding onHover overlay div

--- FrontEnd/src/components/exampleRender.js
+++ FrontEnd/src/components/exampleRender.js
@@ -2,6 +2,7 @@ import styles from "./exampleRender.module.scss";
 import { TiTick } from "react-icons/ti";
 import { ImCross } from "react-icons/im";
 import image from "./logo.png";
+import { IconContext } from "react-icons/lib";
 
 export function ExampleRender(props) {
   let total = 0;
@@ -32,6 +33,28 @@ export function ExampleRender(props) {
         total += 1;
       }
     });
+
+    // Ease of Use Check
+    let easeOfUseScore = 0;
+    if (criteria.criteriaOne[1]) {
+      easeOfUseScore = 1;
+    }
+
+    // Trustworthiness
+    let trustworthiness = 0;
+    if (criteria.criteriaTwo[1] && criteria.criteriaThree[1]) {
+      trustworthiness = 1;
+    }
+    console.log(trustworthiness);
+
+    // Amatuerism
+    let amat = 0;
+
+    const overallScore = {
+      easeOfUse: ["Ease of use", easeOfUseScore],
+      trustworthiness: ["Trustworthiness", trustworthiness],
+    };
+
     // console.log(total);
 
     const URL = props.state[1];
@@ -45,9 +68,35 @@ export function ExampleRender(props) {
           <div className={styles.title}>Example of Rendering with URL</div>
           <div className={styles.render_example}>
             <div className={styles.resultsContainer}>
-              <div>{`${total}/5`}</div>
-              <p>Learn More</p>
-              <div></div>
+              <div className={styles.horizontal}>
+                <div className={styles.score}>{`${total}/5`}</div>
+                <p className={styles.learn_more}>Learn More</p>
+              </div>
+
+              <div>
+                {Object.keys(overallScore).map((key) => {
+                  return (
+                    <div key={key} className={styles.contain}>
+                      <div className={styles.icon}>
+                        <IconContext.Provider
+                          value={
+                            overallScore[key][1]
+                              ? { color: "green", size: 30 }
+                              : { color: "red", size: 20 }
+                          }
+                        >
+                          <div>
+                            {overallScore[key][1] ? <TiTick /> : <ImCross />}
+                          </div>
+                        </IconContext.Provider>
+                      </div>
+                      <div
+                        className={styles.criteria}
+                      >{`${overallScore[key][0]}`}</div>
+                    </div>
+                  );
+                })}
+              </div>
             </div>
             <img src={image} alt="thisisimage" onHover={onHoverHandler} />
             <a href="#">{URL}</a>
