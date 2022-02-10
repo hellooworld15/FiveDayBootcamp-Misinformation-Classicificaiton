96961a82e8094b4266a603a9c81f1faae5608070
Jasmine Warner <hellooworld.warner@gmail.com>
2022-02-11 01:00:26 +1100

    Centered Rendering URL

--- FrontEnd/src/components/exampleRender.js
+++ FrontEnd/src/components/exampleRender.js
@@ -4,7 +4,8 @@ import { ImCross } from "react-icons/im";
 import image from "./logo.png";
 
 export function ExampleRender(props) {
-  console.log(props);
+  let total = 0;
+  //   console.log(props);
   if (props.state) {
     const {
       has_external_sources,
@@ -13,7 +14,7 @@ export function ExampleRender(props) {
       has_typo_errors,
       has_updated,
     } = props.state[0];
-    console.log(has_past_content);
+    // console.log(has_past_content);
 
     const criteria = {
       criteriaOne: ["Website cites external sources", has_external_sources],
@@ -22,17 +23,33 @@ export function ExampleRender(props) {
         "Website contains trusted domain name",
         has_trusted_domain,
       ],
-      criteriaFour: ["Website has typo errors", has_typo_errors],
+      criteriaFour: ["Website has typo errors", !has_typo_errors],
       criteriaFive: ["Website has been updated", has_updated],
     };
+
+    Object.keys(criteria).map((key) => {
+      if (criteria[key][1]) {
+        total += 1;
+      }
+    });
+    // console.log(total);
+
     const URL = props.state[1];
-    console.log(URL);
+    // console.log(URL);
+
+    const onHoverHandler = () => {};
+
     return (
       <>
         <div className={styles.container}>
           <div className={styles.title}>Example of Rendering with URL</div>
           <div className={styles.render_example}>
-            <img src={image} alt="thisisimage" />
+            <div className={styles.resultsContainer}>
+              <div>{`${total}/5`}</div>
+              <p>Learn More</p>
+              <div></div>
+            </div>
+            <img src={image} alt="thisisimage" onHover={onHoverHandler} />
             <a href="#">{URL}</a>
           </div>
         </div>
