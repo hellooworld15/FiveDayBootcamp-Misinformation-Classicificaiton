e3de2ca22ec873dcc942f3d0151b1f19c1f818bb
Jasmine Warner <hellooworld.warner@gmail.com>
2022-02-10 23:18:22 +1100

    Centered Criteria Passed

--- FrontEnd/src/components/criteriaRender.js
+++ FrontEnd/src/components/criteriaRender.js
@@ -22,7 +22,7 @@ export function CriteriaRender(props) {
         "Website contains trusted domain name",
         has_trusted_domain,
       ],
-      criteriaFour: ["Website has typo errors", has_typo_errors],
+      criteriaFour: ["Website has no typographical errors", !has_typo_errors],
       criteriaFive: ["Website has been updated", has_updated],
     };
 
@@ -33,26 +33,58 @@ export function CriteriaRender(props) {
           <ul>
             {Object.keys(criteria).map((key) => {
               //   console.log(criteria[key][1]);
-              return (
-                <li className={styles.row} key={key}>
-                  <div className={styles.contain}>
-                    <div className={styles.icon}>
-                      <IconContext.Provider
-                        value={
-                          criteria[key][1]
-                            ? { color: "green", size: 30 }
-                            : { color: "red", size: 20 }
-                        }
-                      >
-                        <div>{criteria[key][1] ? <TiTick /> : <ImCross />}</div>
-                      </IconContext.Provider>
+              if (criteria[key][1]) {
+                return (
+                  <li className={styles.row} key={key}>
+                    <div className={styles.contain}>
+                      <div className={styles.icon}>
+                        <IconContext.Provider
+                          value={
+                            criteria[key][1]
+                              ? { color: "green", size: 30 }
+                              : { color: "red", size: 20 }
+                          }
+                        >
+                          <div>
+                            {criteria[key][1] ? <TiTick /> : <ImCross />}
+                          </div>
+                        </IconContext.Provider>
+                      </div>
+                      <div
+                        className={styles.criteria}
+                      >{`${criteria[key][0]}`}</div>
                     </div>
-                    <div
-                      className={styles.criteria}
-                    >{`${criteria[key][0]}`}</div>
-                  </div>
-                </li>
-              );
+                  </li>
+                );
+              }
+            })}
+            {/* REALLY BAD PRACTICE - DONT DUPLICATE CODE LIKE THIS  */}
+            {Object.keys(criteria).map((key) => {
+              //   console.log(criteria[key][1]);
+              if (!criteria[key][1]) {
+                return (
+                  <li className={styles.row} key={key}>
+                    <div className={styles.contain}>
+                      <div className={styles.icon}>
+                        <IconContext.Provider
+                          value={
+                            criteria[key][1]
+                              ? { color: "green", size: 30 }
+                              : { color: "red", size: 20 }
+                          }
+                        >
+                          <div>
+                            {criteria[key][1] ? <TiTick /> : <ImCross />}
+                          </div>
+                        </IconContext.Provider>
+                      </div>
+                      <div
+                        className={styles.criteria}
+                      >{`${criteria[key][0]}`}</div>
+                    </div>
+                  </li>
+                );
+              }
             })}
           </ul>
         </div>
